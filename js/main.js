d7a707201934c6f0b78f53fff0e65cd95407de74
poyagorda <byfairen@gmail.com>
2025-05-25 02:02:34 +0200

    Musica
    chuparme el nepe

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("menu-toggle");
-    const menu = document.getElementById("side-menu");
-
-    toggle.addEventListener("click", () => {
-        menu.classList.toggle("open");
-    });
+    
 });
 
 const track = document.querySelector('.slider-track');
@@ -35,5 +31,37 @@ prev.addEventListener('click', () => {
     if (currentIndex > 0) {
         currentIndex--;
         updateSlider();
+    }
+});
+
+document.querySelectorAll('.simple-sidebar a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('menu-toggle').checked = false;
+    });
+});
+
+// Seleccionamos los elementos del lightbox
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".lightbox .close");
+
+// Todas las imágenes del slider o galería
+const galleryImages = document.querySelectorAll('.slider-track img');
+
+galleryImages.forEach(img => {
+    img.addEventListener('click', () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = "none";
+});
+
+// Cerrar al hacer clic fuera de la imagen
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = "none";
     }
 });
