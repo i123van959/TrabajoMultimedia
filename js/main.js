a2138c8742a576cf6adf7aba25c00fa6b3992d73
poyagorda <byfairen@gmail.com>
2025-05-25 02:10:10 +0200

    limpieza y arreglos

--- js/main.js
+++ js/main.js
@@ -11,14 +11,12 @@ const slides = document.querySelectorAll('.slider-track img');
 let currentIndex = 0;
 const visibleSlides = 3;
 
-// Calculamos el ancho real de cada imagen (incluido el gap de 20px)
 const slideWidth = slides[0].clientWidth + 20;
 
 function updateSlider() {
     track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
 }
 
-// Evento flecha derecha
 next.addEventListener('click', () => {
     if (currentIndex < slides.length - visibleSlides) {
         currentIndex++;
@@ -26,7 +24,6 @@ next.addEventListener('click', () => {
     }
 });
 
-// Evento flecha izquierda
 prev.addEventListener('click', () => {
     if (currentIndex > 0) {
         currentIndex--;
@@ -40,12 +37,10 @@ document.querySelectorAll('.simple-sidebar a').forEach(link => {
     });
 });
 
-// Seleccionamos los elementos del lightbox
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.querySelector(".lightbox .close");
 
-// Todas las imágenes del slider o galería
 const galleryImages = document.querySelectorAll('.slider-track img');
 
 galleryImages.forEach(img => {
@@ -59,7 +54,6 @@ closeBtn.addEventListener('click', () => {
     lightbox.style.display = "none";
 });
 
-// Cerrar al hacer clic fuera de la imagen
 lightbox.addEventListener('click', (e) => {
     if (e.target === lightbox) {
         lightbox.style.display = "none";
